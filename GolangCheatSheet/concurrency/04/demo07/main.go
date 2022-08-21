528c4aaddccc3b6ad31d2808369b6e686fafb5dc
StevePro7 <steveprozune@hotmail.com>
2022-08-21 11:34:45 +0100

    no message

--- GolangCheatSheet/concurrency/04/demo07/main.go
+++ GolangCheatSheet/concurrency/04/demo07/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"concurrencytesting/book"
+	"github.com/nikandfor/goid"
+	log "github.com/sirupsen/logrus"
 	"math/rand"
 	"sync"
 	"time"
@@ -11,9 +13,9 @@ var cache = map[int]book.Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func queryCache(id int, m *sync.RWMutex) (book.Book, bool) {
-	m.Lock()
+	m.RLock()
 	b, ok := cache[id]
-	m.Unlock()
+	m.RUnlock()
 	return b, ok
 }
 
@@ -21,9 +23,6 @@ func queryDatabase(id int, m *sync.RWMutex) (book.Book, bool) {
 	time.Sleep(300 * time.Millisecond)
 	for _, b := range book.Books {
 		if b.ID == id {
-			m.Lock()
-			cache[id] = b
-			m.Unlock()
 			return b, true
 		}
 	}
@@ -43,15 +42,43 @@ func main() {
 	for i := 0; i < 2; i++ {
 		id := rnd.Intn(10) + 1
 
-		// Send only channel
-		go func(id, int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- book.Book) {
+		// Send only channel	Query
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- book.Book) {
 			if b, ok := queryCache(id, m); ok {
+				log.Printf("[%d] goroutine send Cache '%s'", goid.ID(), b.Title)
 				ch <- b
 			}
 			wg.Done()
 		}(id, wg, m, cacheCh)
 
-		// Send only channel
+		// Send only channel	Datatbase
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- book.Book) {
+			if b, ok := queryDatabase(id, m); ok {
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				log.Printf("[%d] goroutine send dBase '%s'", goid.ID(), b.Title)
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, dbCh)
 
+		// Receive only channel
+		go func(cacheCh, dbCh <-chan book.Book) {
+
+			select {
+			case b := <-cacheCh:
+				log.Printf("[%d] goroutine recd Cache '%s'", goid.ID(), b.Title)
+				<-dbCh
+
+				case b := <-dbCh:
+					log.Printf("[%d] goroutine recd dBase '%s'", goid.ID(), b.Title)
+			}
+
+		}(cacheCh, dbCh)
 	}
+
+	log.Printf("[%d] running.", goid.ID())
+	wg.Wait()
+	log.Printf("[%d] COMPLETE", goid.ID())
 }
