fc05655f00a38a7de962b81a0dbbfb31b13f4c4e
StevePro7 <steveprozune@hotmail.com>
2022-08-29 17:37:29 +0100

    no message

---
+++ GolangCheatSheetII/00-Concurrency/02-Gorouines/02-CreatingGoroutines/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/nikandfor/goid"
+	"math/rand"
+	"time"
+	
+	log "github.com/sirupsen/logrus"
+)
+
+var cache = map[int]Book{}
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func queryCache(id int) (Book, bool) {
+	b, ok := cache[id]
+	return b, ok
+}
+
+func queryDatabase(id int) (Book, bool) {
+	time.Sleep(300 * time.Millisecond)
+	for _, b := range books {
+		if b.ID == id {
+			cache[id] = b
+			return b, true
+		}
+	}
+
+	return Book{}, false
+}
+
+func main() {
+
+	for i :=0; i < 2; i++ {
+		id := rnd.Intn(10) + 1
+		log.Printf("[%d] ID:%d\n", goid.ID(), id)
+
+		go func(id int) {
+			if b, ok := queryCache(id); ok {
+				log.Printf("[%d] from cache\n", goid.ID())
+				log.Printf("[%d] Book '%v'\n", goid.ID(), b.Title)
+			}
+		}(id)
+
+
+		go func(id int) {
+			if b, ok :=queryDatabase(id); ok {
+				log.Printf("[%d] from database\n", goid.ID())
+				log.Printf("[%d] Book '%v'\n", goid.ID(), b.Title)
+			}
+		}(id)
+	}
+
+	time.Sleep(2 * time.Second)
+}
