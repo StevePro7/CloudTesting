812aa012c508fb7edb00b613f1b55082b98c295a
StevePro7 <steveprozune@hotmail.com>
2023-05-11 12:59:04 +0100

    foo package

--- DockerRemoteDebugging/18-ExampleVSGo3/main.go
+++ DockerRemoteDebugging/18-ExampleVSGo3/main.go
@@ -5,19 +5,16 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"testwebapi/foo"
 	"time"
 )
-
-func foo() int {
-	return 10
-}
 
 func main() {
 	addr := ":8081"
 	log.Printf("Starting server on %s\n", addr)
 	handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		now := time.Now()
-		bar := foo()
+		bar := foo.Foo()
 		msg := fmt.Sprintf("Hello foo '%d' at %s!\n", bar, now.Format("2006-01-02 15:04:05"))
 		_, _ = io.WriteString(w, msg)
 	})
